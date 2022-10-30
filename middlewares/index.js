f4dffa5d0ceb10fdca5e27b12e160941e0f4b8e8
Alejo Ruiz <alejoruizs@hotmail.com>
2022-10-30 00:03:39 -0500

    Add token middlewares

--- middlewares/index.js
+++ middlewares/index.js
@@ -1,4 +1,7 @@
 const {validationResult} = require('express-validator');
+const jwt = require('jsonwebtoken');
+const {token_key} = require('../config/index');
+const {http_response} = require('../config/http_responses/index');
 
 const validate_fields = (req, res, next) => {
   const errors = validationResult(req);
@@ -6,8 +9,35 @@ const validate_fields = (req, res, next) => {
     return res.status(400).json(errors)
   };
   next();
-}
+};
+
+const validate_token = (req, res, next) => {
+  const token = req.header('Authorization');
+  if(!token){
+    return res.status(401).json({msn: 'Unauthorized', error: true})
+  };
+  try {
+    const user = jwt.verify(token, token_key);
+    req.user = user;
+    next();
+  } catch (error) {
+    return res.status(401).json({msn: 'Unauthorized', error: true})
+  }
+};
+
+const admin_role = (req, res, next) => {
+  const {user} = req;
+  if(!user){
+    return res.status(500).json({msn: 'Internal server error', error: true})
+  };
+  if(user.role !== 'Admin'){
+    return res.status(401).json({msn: `${user.email} does not have permission`, error: true});
+  };
+  next();
+};
 
 module.exports = {
-  validate_fields
+  validate_fields,
+  validate_token,
+  admin_role
 }
