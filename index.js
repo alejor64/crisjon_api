b9dd8686427090233b4afad9d29fa765f3b3494f
Alejo Ruiz <alejoruizs@hotmail.com>
2022-10-30 21:04:01 -0500

    Add client endpoints

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 const config = require('./config/index');
 const user_api = require('./api/user/user');
+const client_api = require('./api/client/client');
 let mongo_uri = `mongodb+srv://${config.mongo_user}:${config.mongo_password}@cluster0.${config.mongo_keyword}.mongodb.net/${config.mongo_db}`;
 let options = {
   useNewUrlParser: true,
@@ -15,6 +16,7 @@ app.use(cors());
 app.use(body_parser.urlencoded({extended: true}));
 app.use(body_parser.json());
 app.use('/user', user_api);
+app.use('/client', client_api);
 
 app.get('/', (req, res) => {
   return res.status(200).json({
