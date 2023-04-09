a1356388167feadfd4f689a1ff0bf46debe6c521
Alejo Ruiz <alejoruizs@hotmail.com>
2023-04-08 23:26:28 -0500

    Update order name validation message

--- utils/functions/db_validations/order.js
+++ utils/functions/db_validations/order.js
@@ -2,12 +2,12 @@ const OrderSchema = require('../../../model/order');
 
 const order_by_id = async(id = '') => {
   const order = await OrderSchema.findById(id);
-  if(!order) throw new Error(`The order ${id} does not exist`);
+  if(!order) throw new Error(`The order id ${id} does not exist`);
 };
 
 const order_by_name = async (name = '') => {
   const order = await OrderSchema.findOne({name});
-  if(order) throw new Error(`The order ${name} already exist`);
+  if(order) throw new Error(`The order name ${name} already exist`);
 }
 
 module.exports = {
